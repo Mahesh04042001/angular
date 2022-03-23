eb44692a31fc7d48fabcc3a122d2d35c615a58dd
Maheshwaran S <mahe2998@global.chainsys.com>
2022-03-23 17:31:47 +0530

    interview

--- Challenges/ping-coming-soon-page-master/themes/script.js
+++ Challenges/ping-coming-soon-page-master/themes/script.js
@@ -2,10 +2,18 @@ function ValidateEmail(input) {
   var validRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if (input.value.match(validRegex)) {
+    const message = "Thankyou for your contribution";
+    document.getElementById("msg1").innerHTML = message;
+    document.getElementById("msg1").style.display = "block";
+    document.getElementById("msg").style.display = "none";
+    event.preventDefault();
+
     return true;
   } else {
     const message = "Please provide a email address!";
     document.getElementById("msg").innerHTML = message;
+    document.getElementById("msg").style.display = "block";
+    document.getElementById("msg1").style.display = "none";
     document.myForm.email.focus();
     event.preventDefault();
     return false;
